20a698a3938cf09779fe6a55088a4f2e58665d29
christiansaccani <christian.saccani99@gmail.com>
2024-04-27 15:25:15 +0200

    importato foreign key

---
+++ database/migrations/2024_04_27_131423_add_type_id_foreign_key_to_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // questo ->constrained() obbliga il database (e laravel) a verificare che ogni elmento inserito in quella colonna
+            // abbia l'id corrispettivo nella tabella collegata
+            $table->foreignId('type_id')->nullable()->constrained()->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // qui dobbiamo scrivere il codice che ci annulla l'aggiunta della chiave esterna
+            // nella stringa del parametro di dropForeign() dobbiamo scrivere così:
+            // "tabella_campo_foreign"
+            // questo ci rimuoverà SOLO il vincolo
+            $table->dropForeign('posts_type_id_foreign');
+
+            // poi dobbiamo cancellare la colonna
+            $table->dropColumn('type_id');
+        });
+    }
+};
