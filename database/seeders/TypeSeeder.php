e8d2f938a2573d80af167863e9ec210b3eba4c96
christiansaccani <christian.saccani99@gmail.com>
2024-04-27 15:01:21 +0200

    Seeding table

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,21 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = [
+            'Progetto Front-End',
+            'Progetto Back-End',
+            'Progetto Database',
+        ];
+
+
+        foreach($types as $type) {
+
+            $newType = new Type();
+
+            $newType->title = $type;
+
+            $newType->save();
+
+        }
     }
 }
